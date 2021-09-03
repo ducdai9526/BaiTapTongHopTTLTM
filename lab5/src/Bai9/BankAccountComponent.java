6cd27e6bc13d4fbe305dbefb0be194c63bf17c45
ducdai9526 <ducdai9526@gmail.com>
2021-09-03 10:19:11 +0700

    lab5

---
+++ lab5/src/Bai9/BankAccountComponent.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bai9;
+
+import Bai9.LogView;
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ *
+ * 
+ */
+public class BankAccountComponent extends JComponent{
+    public BankAccountComponent() {
+BankAccount account = new BankAccount();
+LogView view = new LogView("Transaction Log");
+account.addObserver(view);
+BankAccountControl control = new BankAccountControl(account);
+setLayout(new FlowLayout());
+add(view);
+add(control);
+}
+}
---
+++ lab5/src/Bai9/BankAccountFrame.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bai9;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * 
+ */
+public class BankAccountFrame extends JFrame {
+    public BankAccountFrame(){
+        super("Bank Account Test");
+        add(new Bai9.BankAccountComponent());
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    
+    public static void main(String[] args){
+        new BankAccountFrame();
+    }
+}
